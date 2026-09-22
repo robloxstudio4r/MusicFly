c8a1d92ac893b347fdd4aaf9c1bbc937bb901ac3
robloxstudio4r <textaway2@outlook.com>
2026-09-22 14:04:12 -0500

    Add configuration constants for Supabase and app name

---
+++ js/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = "https://zjkhgdifwxqtyhtixngn.supabase.co";
+export const SUPABASE_KEY = "sb_publishable_fdcQPPaNtYrQ6febpRVfyA_KgOuTDrI";
+export const APP_NAME = "MusicFly";
